0c3dfed6d35c4cc0f0d7beb75b8fd3a88aad7e71
shmilyvidian <shmilyvidian@163.com>
2021-06-30 15:17:06 +0800

    feat: add progress with timer&animation&transform

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Progress from './components/Progress/timer';
+// import Progress from './components/Progress/animation';
+// import Progress from './components/Progress/transform';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Progress />
+      </header>
+    </div>
+  );
+}
+
+export default App;
